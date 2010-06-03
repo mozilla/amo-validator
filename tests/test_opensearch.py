e943f2f871e483c56d5489d632c775b1ee51a993
Matt Basta <mbasta@mozilla.com>
2010-06-03 11:51:55 -0700

    Added the last of the unit test resources.

--- tests/test_opensearch.py
+++ tests/test_opensearch.py
@@ -22,50 +22,50 @@ def _do_test(url, failure=True):
 def test_opensearch():
     "Tests that the OpenSearch detection is working."
     
-    _do_test("tests/resources/searchprovider/search_provider.xml", False)
+    _do_test("tests/resources/searchprovider/pass.xml", False)
     
 def test_broken_updateURL():
     "Tests that there isn't an updateURL element in the provider."
     
-    _do_test("tests/resources/searchprovider/search_provider_updateurl.xml")
+    _do_test("tests/resources/searchprovider/sp_updateurl.xml")
     
 def test_broken_notos():
     "Tests that the provider is indeed OpenSearch."
     
-    _do_test("tests/resources/searchprovider/search_provider_notos.xml")
+    _do_test("tests/resources/searchprovider/sp_notos.xml")
     
 def test_broken_shortname():
     "Tests that the provider has a <ShortName> element."
     
-    _do_test("tests/resources/searchprovider/search_provider_shortname.xml")
+    _do_test("tests/resources/searchprovider/sp_no_shortname.xml")
     
 def test_broken_description():
     "Tests that the provider has a <Description> element."
     
-    _do_test("tests/resources/searchprovider/search_provider_description.xml")
+    _do_test("tests/resources/searchprovider/sp_no_description.xml")
     
 def test_broken_url():
     "Tests that the provider has a <Url> element."
     
-    _do_test("tests/resources/searchprovider/search_provider_url.xml")
+    _do_test("tests/resources/searchprovider/sp_no_url.xml")
     
 def test_broken_url_mime():
     "Tests that the provider has a legit MIME on the URL."
     
-    _do_test("tests/resources/searchprovider/search_provider_url_mime.xml")
+    _do_test("tests/resources/searchprovider/sp_bad_url_mime.xml")
     
 def test_broken_url_searchterms():
     "Tests that a search term field is provided for the <Url> element."
     
-    _do_test("tests/resources/searchprovider/search_provider_url_term.xml")
+    _do_test("tests/resources/searchprovider/sp_no_url_template.xml")
     
 def test_broken_url_searchterms_inline():
     "Tests that a valid inline search term field is provided."
     
-    _do_test("tests/resources/searchprovider/search_provider_url_term2.xml", False)
+    _do_test("tests/resources/searchprovider/sp_inline_template.xml", False)
     
 def test_broken_url_searchterms_param():
     "Tests that a valid search term field is provided in a <Param />"
     
-    _do_test("tests/resources/searchprovider/search_provider_url_term3.xml", False)
+    _do_test("tests/resources/searchprovider/sp_param_template.xml", False)
     
--- tests/test_xpimanager.py
+++ tests/test_xpimanager.py
@@ -4,24 +4,21 @@ from xpi import XPIManager
 
 def test_open():
     "Test that the manager will open the package"
-    try:
-        z = XPIManager("testfiles/install_rdf_only.xpi")
-    except:
-        assert False
+    z = XPIManager("tests/resources/xpi/install_rdf_only.xpi")
     assert z is not None
     
 def test_get_list():
     "Test that the manager can read the file listing"
-    z = XPIManager("testfiles/install_rdf_only.xpi")
+    z = XPIManager("tests/resources/xpi/install_rdf_only.xpi")
     assert z.get_file_data()
     
 def test_valid_name():
     "Test that the manager can retrieve the correct file name"
-    z = XPIManager("testfiles/install_rdf_only.xpi")
+    z = XPIManager("tests/resources/xpi/install_rdf_only.xpi")
     contents = z.get_file_data()
     assert "install.rdf" in contents
     
 def test_read_file():
     "Test that a file can be read from the package"
-    z = XPIManager("testfiles/install_rdf_only.xpi")
+    z = XPIManager("tests/resources/xpi/install_rdf_only.xpi")
     assert z.read("install.rdf") is not None
