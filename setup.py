7ea017861e2d9b616a8dfc574ea689542b397ff2
Matt Basta <mbasta@mozilla.com>
2010-07-01 14:47:47 -0700

    Added a setup.py setup script

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='amo-validator',
+    version='1.0',
+    description='Validates addons for Mozilla products.',
+    long_description=open('README.rst').read(),
+    author='Matt Basta',
+    author_email='me@mattbasta.com',
+    url='http://github.com/mattbasta/amo-validator',
+    license='BSD',
+    packages=find_packages(exclude=['tests',
+                                    'tests/*',
+                                    'extras',
+                                    'extras/*']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['nose',
+                      'cssutils',
+                      'rdflib'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
