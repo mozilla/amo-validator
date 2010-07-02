02a04bcc4fc89c3957d5f4d16b6002f15e5d87b3
Matt Basta <mbasta@mozilla.com>
2010-07-02 10:24:04 -0700

    Added scripts directive

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ setup(
     install_requires=['nose',
                       'cssutils',
                       'rdflib'],
+    scripts=["validator.py"],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
