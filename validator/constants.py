8c0060bb06dafe40ee8a2533e4eef56f71c62488
Nils Maier <maierman@web.de>
2011-01-26 19:11:42 +0100

    Let SPIDERMONKEY_INSTALLATION be an environment variable
    This is a simple solution which lets the user specify the spidermonkey
    installation to actually use, e.g. an easy way to test different builds
    easily.
    
    On windows it is js.exe, and the current code is unable to detect that.
    So profit for windows users

--- validator/constants.py
+++ validator/constants.py
@@ -15,11 +15,14 @@ PACKAGE_SUBPACKAGE = 7
 # The "earliest" version number for Firefox 4
 FF4_MIN = "3.7a1pre"
 
+SPIDERMONKEY_INSTALLATION = os.environ.get("SPIDERMONKEY_INSTALLATION")
+
 # Graciously provided by @kumar in bug 614574
-for p in os.environ.get('PATH', '').split(':'):
-    SPIDERMONKEY_INSTALLATION = os.path.join(p, "js")
-    if os.path.exists(os.path.join(p, SPIDERMONKEY_INSTALLATION)):
-        break
+if not os.path.exists(SPIDERMONKEY_INSTALLATION):
+    for p in os.environ.get('PATH', '').split(':'):
+        SPIDERMONKEY_INSTALLATION = os.path.join(p, "js")
+        if os.path.exists(os.path.join(p, SPIDERMONKEY_INSTALLATION)):
+            break
 
 if not os.path.exists(SPIDERMONKEY_INSTALLATION):
     
