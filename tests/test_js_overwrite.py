f8cc797cc034a43f8a5e68c0baf97b450190295f
mattbasta <me@mattbasta.com>
2011-02-18 05:11:16 -0500

    Added additional unit test for global instance overwrite

--- tests/test_js_overwrite.py
+++ tests/test_js_overwrite.py
@@ -10,3 +10,13 @@ def test_new_overwrite():
     """)
     assert not results.message_count
 
+def test_redefine_new_instance():
+    "Test the redefinition of an instance of a global type."
+
+    results = _do_test_raw("""
+    var foo = "asdf";
+    var r = new RegEx(foo, "i");
+    r = new RegExp(foo, "i");
+    r = null;
+    """)
+    assert not results.message_count
