cdcee58b03c18eafcf766cc03490c85b79bd7701
Wil Clouser <wclouser@mozilla.com>
2011-03-07 14:07:27 -0800

    Add build script for hudson

---
+++ scripts/build.sh
@@ -0,0 +1,34 @@
+# This script should be called from within Hudson
+
+cd $WORKSPACE
+VENV=$WORKSPACE/venv
+
+echo "Starting build on executor $EXECUTOR_NUMBER..." `date`
+
+if [ -z $1 ]; then
+    echo "Warning: You should provide a unique name for this job to prevent database collisions."
+    echo "Usage: ./build.sh <name>"
+    echo "Continuing, but don't say you weren't warned."
+fi
+
+echo "Setup..." `date`
+
+# Make sure there's no old pyc files around.
+find . -name '*.pyc' | xargs rm
+
+if [ ! -d "$VENV/bin" ]; then
+  echo "No virtualenv found.  Making one..."
+  virtualenv $VENV
+fi
+
+source $VENV/bin/activate
+
+pip install -q -r requirements.txt
+
+export SPIDERMONKEY_INSTALLATION="/usr/local/bin/tracemonkey"
+
+echo "Starting tests..." `date`
+
+nosetests --with-coverage --cover-package=validator --cover-skip=validator.outputhandlers.,validator.main,validator.constants,validator.constants_local --cover-inclusive --cover-tests
+
+echo 'shazam!'
