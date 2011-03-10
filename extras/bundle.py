85bba7c1889dcc25a3edd8b2377561c96e307d1b
Matt Basta <me@mattbasta.com>
2011-03-10 23:35:05 +0000

    Merged updated content test

--- extras/bundle.py
+++ extras/bundle.py
@@ -11,32 +11,32 @@ zf = ZipFile(target, 'w')
 
 def _build_directory(source, zip, root):
     for item in os.listdir(source):
-        
+
         if item in ("__MACOSX",
                     ".DS_Store"):
             continue
-        
+
         if item.startswith("__"):
             continue
-        
+
         item = "%s/%s" % (source, item)
         print item
-        
+
         if os.path.isdir(item):
-            if item.endswith(".jar"):
+            if item.startswith("_") and item.endswith(".jar"):
                 zipbuffer = StringIO()
                 subzip = ZipFile(zipbuffer, "w")
                 _build_directory(item, subzip, item)
                 subzip.close()
-                
-                zip.writestr(item[len(root) + 1:], zipbuffer.getvalue())
+
+                zip.writestr(item[len(root) + 2:], zipbuffer.getvalue())
             else:
                 _build_directory(item, zip, root)
         else:
             print item[len(root) + 1:]
             zip.write(item, item[len(root) + 1:])
-        
-        
+
+
 
 _build_directory(source, zf, source)
 zf.close()
