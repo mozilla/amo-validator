b6c800a8d488b5b452ddd0ffa7238a8d174d17bf
Matt Basta <me@mattbasta.com>
2011-03-31 00:19:45 +0000

    Added Unicode support for JS and error output

---
+++ validator/unicodehelper.py
@@ -0,0 +1,40 @@
+import codecs
+import textfilter
+
+# Many thanks to nmaier for inspiration and code in this module
+
+UNICODES = [
+    (codecs.BOM_UTF8, "utf-8"),
+    (codecs.BOM_UTF32_LE, "utf-32-le"),
+    (codecs.BOM_UTF32_BE, "utf-32-be"),
+    (codecs.BOM_UTF16_LE, "utf-16-le"),
+    (codecs.BOM_UTF16_BE, "utf-16-be"),
+    ]
+
+COMMON_ENCODINGS = ("utf-8", "utf-16", "latin_1", "ascii")
+
+def decode(data):
+    """
+    Decode data employing some charset detection and including unicode BOM
+    stripping.
+    """
+
+    # Don't make more work than we have to.
+    if not isinstance(data, str):
+        return data
+
+    # Detect standard unicodes.
+    for bom, encoding in UNICODES:
+        if data[:len(bom) - 1] == bom:
+            return unicode(data[len(bom):], encoding, "ignore")
+
+    # Test for various common encodings.
+    for encoding in COMMON_ENCODINGS:
+        try:
+            return unicode(data, encoding)
+        except UnicodeDecodeError:
+            pass
+
+    # Anything else gets filtered.
+    return unicode(textfilter.filter_ascii(data))
+
