4a677ff42ec8d33ad083369159e279e1beb8928d
Matt Basta <me@mattbasta.com>
2011-06-02 11:52:47 -0700

    Fixes bug 661576; changes the type of message that's produced and sets the compatibility to only 3.7-4.0.*

--- validator/testcases/compatibility.py
+++ validator/testcases/compatibility.py
@@ -3,10 +3,17 @@ import validator.decorator as decorator
 from validator.decorator import versions_after
 from validator.constants import *
 
+FX4_VERSIONS = ["3.7a1pre", "3.7a1", "3.7a2pre", "3.7a2", "3.7a3pre", "3.7a3",
+                "3.7a4pre", "3.7a4", "3.7a5pre", "3.7a5", "3.7a6pre", "4.0b1",
+                "4.0b2pre", "4.0b2", "4.0b3pre", "4.0b3", "4.0b4pre", "4.0b4",
+                "4.0b5pre", "4.0b5", "4.0b6pre", "4.0b6", "4.0b7pre", "4.0b7",
+                "4.0b8pre", "4.0b8", "4.0b9pre", "4.0b9", "4.0b10pre",
+                "4.0b10", "4.0b11pre", "4.0b11", "4.0b12pre", "4.0b12", "4.0",
+                "4.0.*"]
 
 @decorator.register_test(tier=5,
                          versions={"{ec8030f7-c20a-464f-9b0e-13a3a9e97384}":
-                                       versions_after("firefox", "4.2a1pre")})
+                                       FX4_VERSIONS})
 def navigator_language(err, xpi):
     """Any use of `navigator.language` should be flagged."""
 
@@ -16,10 +23,10 @@ def navigator_language(err, xpi):
         return
 
     for filename, line, context in compat_references["navigator_language"]:
-        err.warning(
+        err.notice(
             err_id=("testcases_compatibility",
                     "navigator_language"),
-            warning="navigator.language may not behave as expected",
+            notice="navigator.language may not behave as expected",
             description="JavaScript code was found that references "
                         "navigator.language, which will no longer indicate "
                         "the language of Firefox's UI. To maintain existing "
