a3698e280b8631586d6616eebd221142f2b3356c
Andrei Hajdukewycz <sancus@off.net>
2012-07-03 19:41:37 -0700

    add Thunderbird 14 compatibility tests for bug759977

---
+++ tests/compat/test_tb14.py
@@ -0,0 +1,33 @@
+from helper import CompatTestCase
+from validator.compat import TB14_DEFINITION
+
+
+class TestTB14Compat(CompatTestCase):
+    """
+    Test that compatibility tests for Thunderbird 14 are properly executed.
+    """
+
+    VERSION = TB14_DEFINITION
+
+    def test_interfaces(self):
+        for method in self.run_xpcom_for_compat(
+                "nsIMsgPluggableStore", ["copyMessages()"]):
+            self.assert_silent()
+            self.assert_compat_error(type_="notice")
+
+    def test_functions(self):
+        patterns = ["mailnews.display.html_sanitizer.allowed_tags", "gPrefs()",
+         "gHeaderParser()", "msgComposeService()", "nsPrefBranch()",]
+        for pattern in patterns:
+            self.run_regex_for_compat("var x = %s();" % pattern)
+            self.assert_silent()
+            assert self.compat_err.notices
+            assert self.compat_err.compat_summary["errors"]
+
+        # Extra tests for similar functions.
+        for pattern in ["cvsPrefs()", "mailnews()", 
+                        "CollapseSectionHeaderators()",]:
+            self.run_regex_for_compat("var x = %s();" % pattern)
+            self.assert_silent()
+            self.assert_compat_silent()
+
