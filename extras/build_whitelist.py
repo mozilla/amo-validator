d2763ca83746363a6e58f037a3872fa289c5625b
Matt Basta <me@mattbasta.com>
2012-08-29 10:41:09 -0700

    Updated hashes from @eviljeff's updates.

--- extras/build_whitelist.py
+++ extras/build_whitelist.py
@@ -3,15 +3,12 @@ import os
 import os.path as pth
 import hashlib
 
-output = open("whitelist_hashes.txt", mode="w")
-
-for root, dirs, files in os.walk(sys.argv[1]):
-    for filename in files:
-        path = pth.join(pth.dirname(pth.abspath(sys.argv[0])),
-                        root, filename)
-        hash = hashlib.sha1(open(path).read()).hexdigest()
-        print path, hash
-        output.write(hash + "\n")
-
-output.close()
+with open("whitelist_hashes.txt", mode="w") as output:
+    for root, dirs, files in os.walk(sys.argv[1]):
+        for filename in files:
+            path = pth.join(pth.dirname(pth.abspath(sys.argv[0])),
+                            root, filename)
+            hash = hashlib.sha1(open(path).read()).hexdigest()
+            print path, hash
+            output.write(hash + "\n")
 
