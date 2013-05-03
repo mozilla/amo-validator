624e008ff60de324087642c1d52af38ce6ced8eb
Andreas Wagner <mail@andreaswagner.org>
2013-05-03 21:07:39 +0200

    Fixed regex of js patterns for testcases

--- tests/compat/test_tb18.py
+++ tests/compat/test_tb18.py
@@ -9,15 +9,19 @@ class TestTB18Compat(CompatTestCase):
 
     def test_js_patterns(self):
         """Test that these js patterns are flagged in Thunderbird 18."""
-        def test_pattern(self, pattern):
-            self.setUp()
-            self.run_regex_for_compat("var x = %s();" % pattern)
-            self.assert_compat_error(type_="notice")
-
-        yield test_pattern, self, "queryISupportsArray"
+        self.setUp()
+        self.run_regex_for_compat("var x = %s();" % "queryISupportsArray")
+        self.assert_compat_error(type_="notice")
 
     def test_prplIAccount_noNewlines(self):
         for method in self.run_xpcom_for_compat(
             "prplIAccount", ["noNewlines"]):
             self.assert_silent()
             self.assert_compat_error(type_="notice")
+
+    def test_unflagged_patterns(self):
+        """Test that these js patterns are _NOT_ flagged in Thunderbird 18."""
+        self.setUp()
+        self.run_regex_for_compat("var x = %s();" % "IncomingServer")
+        self.assert_silent()
+        self.assert_compat_silent()
