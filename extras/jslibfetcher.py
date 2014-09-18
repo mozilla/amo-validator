bb05fa0c23c6632e52f4b08bb3f2b03377322f7b
erosman <erosman@users.noreply.github.com>
2014-09-18 16:32:54 +0430

    Update jslibfetcher.py

--- extras/jslibfetcher.py
+++ extras/jslibfetcher.py
@@ -229,6 +229,59 @@ YUI_NEW_VERSIONS = [
     "3.6.0",
 ]
 
+# AngularJS (Google), no rc, no beta
+ANGULARJS_VERSIONS = [
+    "1.0.1",
+    "1.0.2",
+    "1.0.3",
+    "1.0.4",
+    "1.0.5",
+    "1.0.6",
+    "1.0.7",
+    "1.0.8",
+    "1.1.0",
+    "1.1.1",
+    "1.1.2",
+    "1.1.3",
+    "1.1.4",
+    "1.1.5",
+    "1.2.0",
+    "1.2.1",
+    "1.2.2",
+    "1.2.3",
+    "1.2.4",
+    "1.2.5",
+    "1.2.6",
+    "1.2.7",
+    "1.2.8",
+    "1.2.9",
+    "1.2.10",
+    "1.2.11",
+    "1.2.12",
+    "1.2.13",
+    "1.2.14",
+    "1.2.15",
+    "1.2.16",
+    "1.2.17",
+    "1.2.18",
+    "1.2.19",
+    "1.2.20",
+    "1.2.21",
+    "1.2.22",
+    "1.2.23",
+    "1.2.24",
+    "1.2.25",
+]
+
+
+# AngularJS (Google)
+get_pattern("angularjs",
+            "https://ajax.googleapis.com/ajax/libs/angularjs/%s/angular.js",
+            ANGULARJS_VERSIONS)
+get_pattern("angularjs",
+            "https://ajax.googleapis.com/ajax/libs/angularjs/%s/angular.min.js",
+            ANGULARJS_VERSIONS)
+
 
 # Dojo Toolkit
 get_pattern("dojo",
