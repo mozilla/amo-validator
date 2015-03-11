f96c0d654ad6c06fbbcce2ab343c3e2dd50fc5d9
Mark Striemer <mstriemer@mozilla.com>
2015-03-11 10:56:40 -0500

    Warn when calling a method called `quote` (bug 1134367)

---
+++ tests/compat/test_gecko37.py
@@ -0,0 +1,32 @@
+from helper import CompatTestCase
+from validator.compat import FX37_DEFINITION
+
+
+class TestFX37Compat(CompatTestCase):
+    """Test that compatibility tests for Gecko 37 are properly executed."""
+
+    VERSION = FX37_DEFINITION
+
+    def test_string_quote_variable(self):
+        self.run_script_for_compat("""
+            var myString = "A string!";
+            console.log(myString.quote());
+        """)
+        self.assert_silent()
+        self.assert_compat_error()
+
+    def test_string_quote_argument(self):
+        self.run_script_for_compat("""
+            function printString(myString) {
+                console.log(myString.quote());
+            }
+        """)
+        self.assert_silent()
+        self.assert_compat_error()
+
+    def test_string_quote_literal(self):
+        self.run_script_for_compat("""
+            console.log("A string!".quote());
+        """)
+        self.assert_silent()
+        self.assert_compat_error()
