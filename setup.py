78cc3b7a4fdb7d367966361f3e0f83676968b1f8
Mathieu Agopian <mathieu@agopian.info>
2015-06-11 22:52:19 +0200

    1.7.6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='amo-validator',
-    version='1.7.5',
+    version='1.7.6',
     description='Validates add-ons for Mozilla products.',
     long_description=open('README.md').read(),
     author='Matt Basta',
