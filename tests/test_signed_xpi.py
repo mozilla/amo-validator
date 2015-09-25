c8626939c045e3dda2a78255ae7352c2d37e4efa
Mark Striemer <mstriemer@mozilla.com>
2015-09-25 15:41:13 -0500

    Require a signature for all inner packages

--- tests/test_signed_xpi.py
+++ tests/test_signed_xpi.py
@@ -112,17 +112,3 @@ def test_other_signed_multi_xpi():
     assert not err.warnings
     assert err.errors
     eq_(err.errors[0]['id'], ('testcases_content', 'unsigned_sub_xpi'))
-
-
-def test_unsigned_inner_xpi():
-    """Test that inner packages don't need to be signed if not multipackage."""
-    xpi = MockXPI(subpackage=True)
-
-    err = ErrorBundle()
-    err.supported_versions = {}
-    err.push_state('inner.jar')
-    err.save_resource('is_multipackage', False)
-
-    content.test_signed_xpi(err, xpi)
-
-    assert not err.failed()
