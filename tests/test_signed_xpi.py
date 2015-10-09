a3ce9a714e1d186d14c140b2e0a54a0b472d9cba
Mark Striemer <mstriemer@mozilla.com>
2015-10-09 15:36:05 -0500

    Don't require signed inner JAR when not multipackage (bug 1213163)
    Require all inner XPI files to be signed but not JAR files.
    Fixes #331

--- tests/test_signed_xpi.py
+++ tests/test_signed_xpi.py
@@ -112,3 +112,32 @@ def test_other_signed_multi_xpi():
     assert not err.warnings
     assert err.errors
     eq_(err.errors[0]['id'], ('testcases_content', 'unsigned_sub_xpi'))
+
+
+def test_unsigned_inner_xpi():
+    """Test that inner XPIs need to be signed even if not multi-package."""
+    xpi = MockXPI(subpackage=True)
+
+    err = ErrorBundle()
+    err.supported_versions = {}
+    err.push_state('inner.xpi')
+    err.save_resource('is_multipackage', False)
+
+    content.test_signed_xpi(err, xpi)
+
+    assert err.failed()
+
+
+def test_unsigned_inner_jar():
+    """Test that inner packages don't need to be signed if not multipackage."""
+    xpi = MockXPI(subpackage=True)
+    xpi.filename = 'mock_jar.jar'
+
+    err = ErrorBundle()
+    err.supported_versions = {}
+    err.push_state('inner.jar')
+    err.save_resource('is_multipackage', False)
+
+    content.test_signed_xpi(err, xpi)
+
+    assert not err.failed()
