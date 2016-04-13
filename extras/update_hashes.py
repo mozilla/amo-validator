fca690a8dcf06f7c44fbc8cd9eb04ae4baa40789
Andreas Wagner <mail@andreaswagner.org>
2016-04-13 18:24:15 +0200

    Add warning list for third party libraries

---
+++ extras/update_hashes.py
@@ -0,0 +1,14 @@
+import glob
+import os
+import subprocess
+
+path = os.path.dirname(__file__)
+target = "jslibs"
+
+subprocess.call(["python", os.path.join(path, "jslibfetcher.py"), target])
+subprocess.call(["python", os.path.join(path, "build_hashes.py"), target])
+
+for file in glob.glob(os.path.join(path, target, "hashes-*.txt")):
+    os.rename(file, os.path.join(path, os.path.pardir, "validator", "testcases", os.path.basename(file)))
+
+print "\nThird party library hashes updated."
