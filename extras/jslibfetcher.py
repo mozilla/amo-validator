c33e2b89a8a07987414a639086f02edda2f54e95
Andreas Wagner <mail@andreaswagner.org>
2016-04-15 18:42:35 +0200

    Cleanup JS third-party lib fetching

--- extras/jslibfetcher.py
+++ extras/jslibfetcher.py
@@ -1,327 +1,14 @@
-import urllib
 import os
+import requests
 import sys
 
+download_errors = []
 
 ALLOWED = "allowed"
 WARNING = "warning"
 
-
-def process(url, rule, file):
-    dest_folder = os.path.join(os.path.abspath(os.path.dirname(__file__)),
-                               sys.argv[1], rule)
-
-    if not os.path.exists(dest_folder):
-        os.makedirs(dest_folder)
-
-    destination = os.path.join(dest_folder, file)
-    if os.path.exists(destination):
-        return
-
-    try:
-        print url
-        urllib.urlretrieve(url, destination)
-    except Exception as e:
-        print "Failed", e
-
-
-def get_pattern(prefix, url_pattern, library):
-    for rule, versions in library.iteritems():
-        for version in versions:
-            url = url_pattern % version
-            process(url, rule, "%s.%s.%s" % (prefix, version, url.split("/")[-1]))
-
-
-process("https://addons.cdn.mozilla.net/en-US/firefox/files/browse/149703/"
-        "file-serve/bootstrap.js?token=f885db28-1ec2-419d-8b35-72300523befb",
-        ALLOWED, "PersonasInteractive_bootstrap.js")
-
-
-BACKBONE_VERSIONS = {
-    ALLOWED: [
-        "1.0.0",
-        "1.1.0",
-        "1.1.1",
-        "1.1.2",
-    ]
-}
-
-BACKBONE_LOCALSTORAGE_VERSIONS = {
-    ALLOWED: [
-        "1.1.10",
-        "1.1.12",
-        "1.1.13",
-        "1.1.14",
-        "1.1.15",
-        "1.1.16",
-    ]
-}
-
-DOJO_VERSIONS = {
-    ALLOWED: [
-        "1.4.0",
-        "1.4.1",
-        "1.4.3",
-        "1.5",
-        "1.5.1",
-        "1.6",
-        "1.6.1",
-        "1.7.0",
-        "1.7.1",
-        "1.7.2",
-        "1.7.3",
-        "1.8.0",
-        "1.8.1",
-        "1.8.2",
-        "1.8.3",
-        "1.8.4",
-        "1.8.5",
-        "1.8.6",
-        "1.9.0",
-        "1.9.1",
-        "1.9.2",
-        "1.9.3",
-    ]
-}
-
-# Google doesn't yet host EXT4.*
-EXT_VERSIONS = {
-    ALLOWED: [
-        "3.0.0",
-        "3.1.0",
-    ]
-}
-
-JQUERY_VERSIONS = {
-    ALLOWED: [
-        "1.0.1",
-        "1.0.2",
-        "1.0.3",
-        "1.0.4",
-        "1.1",
-        "1.1.1",
-        "1.1.2",
-        "1.4",
-        "1.4.1",
-        "1.4.2",
-        "1.4.4",
-        "1.5",
-        "1.5.1",
-        "1.5.2",
-        "1.6",
-        "1.6.1",
-        "1.6.2",
-        "1.6.3",
-        "1.6.4",
-        "1.7",
-        "1.7.1",
-        "1.7.2",
-        "1.8.0",
-        "1.8.1",
-        "1.8.2",
-        "1.8.3",
-        "1.9.0",
-        "1.9.1",
-        "1.10.0",
-        "1.10.1",
-        "1.10.2",
-        "1.11.0",
-        "1.11.1",
-        "1.11.2",
-        "1.11.3",
-        "1.12.0",
-        "1.12.1",
-        "1.12.2",
-        "2.0.0",
-        "2.0.1",
-        "2.0.2",
-        "2.0.3",
-        "2.1.0",
-        "2.1.1",
-        "2.1.2",
-        "2.1.3",
-        "2.1.4",
-        "2.2.0",
-        "2.2.1",
-        "2.2.2",
-    ]
-}
-
-JQUERYUI_VERSIONS = {
-    ALLOWED: [
-        "1.5.2",
-        "1.5.3",
-        "1.6.0",
-        "1.7.0",
-        "1.7.1",
-        "1.7.2",
-        "1.7.3",
-        "1.8.0",
-        "1.8.1",
-        "1.8.2",
-        "1.8.4",
-        "1.8.5",
-        "1.8.6",
-        "1.8.7",
-        "1.8.8",
-        "1.8.9",
-        "1.8.10",
-        "1.8.11",
-        "1.8.12",
-        "1.8.13",
-        "1.8.14",
-        "1.8.15",
-        "1.8.16",
-        "1.8.17",
-        "1.8.18",
-        "1.8.19",
-        "1.8.20",
-        "1.8.21",
-        "1.8.22",
-        "1.8.23",
-        "1.8.24",
-        "1.9.0",
-        "1.9.1",
-        "1.9.2",
-        "1.10.0",
-        "1.10.1",
-        "1.10.2",
-        "1.10.3",
-        "1.10.4",
-        "1.11.0",
-        "1.11.1",
-        "1.11.2",
-        "1.11.3",
-        "1.11.4",
-    ]
-}
-
-MOMENTJS_VERSIONS = {
-    ALLOWED: [
-        "2.10.6",
-        "2.10.5",
-        "2.10.3",
-        "2.10.2",
-        "2.9.0",
-    ]
-}
-
-MOOTOOLS_VERSIONS = {
-    ALLOWED: [
-        "1.1.1",
-        "1.1.2",
-        "1.2.1",
-        "1.2.2",
-        "1.2.3",
-        "1.2.4",
-        "1.2.5",
-        "1.3.0",
-        "1.3.1",
-        "1.3.2",
-        "1.4.0",
-        "1.4.1",
-        "1.4.2",
-        "1.4.3",
-        "1.4.4",
-        "1.4.5",
-        "1.5.0",
-    ]
-}
-
-PROTOTYPE_VERSIONS = {
-    ALLOWED: [
-        "1.6.0.2",
-        "1.6.0.3",
-        "1.6.1.0",
-        "1.7.0.0",
-        "1.7.1.0",
-        "1.7.2.0",
-    ]
-}
-
-SCRIPTACULOUS_VERSIONS = {
-    ALLOWED: [
-        "1.8.1",
-        "1.8.2",
-        "1.8.3",
-        "1.9.0",
-    ]
-}
-
-SWFOBJECT_VERSIONS = {
-    ALLOWED: [
-        "2.1",
-        "2.2",
-    ]
-}
-
-UNDERSCORE_VERSIONS = {
-    ALLOWED: [
-        "1.8.3",
-        "1.8.2",
-        "1.8.1",
-        "1.8.0",
-        "1.7.0",
-        "1.6.0",
-        "1.5.2",
-        "1.5.1",
-        "1.5.0",
-        "1.4.4",
-        "1.4.3",
-        "1.4.2",
-        "1.4.1",
-        "1.4.0",
-        "1.3.3",
-        "1.3.2",
-        "1.3.1",
-        "1.3.0",
-        "1.2.4",
-        "1.2.3",
-        "1.2.2",
-        "1.2.1",
-        "1.2.0",
-        "1.1.7",
-        "1.1.6",
-        "1.1.5",
-        "1.1.4",
-        "1.1.3",
-        "1.1.2",
-        "1.1.1",
-        "1.1.0",
-        "1.0.4",
-        "1.0.3",
-        "1.0.2",
-        "1.0.1",
-        "1.0.0",
-    ]
-}
-
-YUI_VERSIONS = {
-    ALLOWED: [
-        "2.6.0",
-        "2.7.0",
-        "2.8.0r4",
-        "2.8.1",
-        "2.8.2",
-        "2.9.0",
-    ]
-}
-
-YUI_NEW_VERSIONS = {
-    ALLOWED: [
-        "3.3.0",
-        "3.4.0",
-        "3.4.1",
-        "3.5.0",
-        "3.5.1",
-        "3.6.0",
-    ]
-}
-
-# AngularJS (Google), no rc, no beta
 ANGULARJS_VERSIONS = {
     WARNING: [
-        "1.0.1",
         "1.0.2",
         "1.0.3",
         "1.0.4",
@@ -396,142 +83,313 @@ ANGULARJS_VERSIONS = {
         "1.4.7",
         "1.4.8",
         "1.4.9",
+        "1.4.10",
         "1.5.0",
         "1.5.1",
         "1.5.2",
-    ],
-    ALLOWED: [
         "1.5.3",
     ],
+    ALLOWED: [
+        "1.5.4",
+    ],
+}
+
+BACKBONE_VERSIONS = {
+    ALLOWED: [
+        "1.0.0",
+        "1.1.0",
+        "1.1.1",
+        "1.1.2",
+    ]
+}
+
+DOJO_VERSIONS = {
+    ALLOWED: [
+        "1.5.0",
+        "1.5.1",
+        "1.6.0",
+        "1.6.1",
+        "1.7.0",
+        "1.7.1",
+        "1.7.2",
+        "1.7.3",
+        "1.8.0",
+        "1.8.1",
+        "1.8.2",
+        "1.8.3",
+        "1.8.4",
+        "1.8.5",
+        "1.8.6",
+        "1.9.0",
+        "1.9.1",
+        "1.9.2",
+        "1.9.3",
+    ]
+}
+
+JQUERY_VERSIONS = {
+    ALLOWED: [
+        "1.5",
+        "1.5.1",
+        "1.5.2",
+        "1.6",
+        "1.6.1",
+        "1.6.2",
+        "1.6.3",
+        "1.6.4",
+        "1.7",
+        "1.7.1",
+        "1.7.2",
+        "1.8.0",
+        "1.8.1",
+        "1.8.2",
+        "1.8.3",
+        "1.9.0",
+        "1.9.1",
+        "1.10.0",
+        "1.10.1",
+        "1.10.2",
+        "1.11.0",
+        "1.11.1",
+        "1.11.2",
+        "1.11.3",
+        "1.12.0",
+        "1.12.1",
+        "1.12.2",
+        "1.12.3",
+        "2.0.0",
+        "2.0.1",
+        "2.0.2",
+        "2.0.3",
+        "2.1.0",
+        "2.1.1",
+        "2.1.2",
+        "2.1.3",
+        "2.1.4",
+        "2.2.0",
+        "2.2.1",
+        "2.2.2",
+        "2.2.3",
+    ]
+}
+
+JQUERYUI_VERSIONS = {
+    ALLOWED: [
+        "1.8.8",
+        "1.8.9",
+        "1.8.10",
+        "1.8.11",
+        "1.8.12",
+        "1.8.13",
+        "1.8.14",
+        "1.8.15",
+        "1.8.16",
+        "1.8.17",
+        "1.8.18",
+        "1.8.19",
+        "1.8.20",
+        "1.8.21",
+        "1.8.22",
+        "1.8.23",
+        "1.8.24",
+        "1.9.0",
+        "1.9.1",
+        "1.9.2",
+        "1.10.0",
+        "1.10.1",
+        "1.10.2",
+        "1.10.3",
+        "1.10.4",
+        "1.11.0",
+        "1.11.1",
+        "1.11.2",
+        "1.11.3",
+        "1.11.4",
+    ]
+}
+
+MOMENTJS_VERSIONS = {
+    ALLOWED: [
+        "2.9.0",
+        "2.10.2",
+        "2.10.3",
+        "2.10.5",
+        "2.10.6",
+    ]
+}
+
+MOOTOOLS_VERSIONS = {
+    ALLOWED: [
+        "1.3.1",
+        "1.3.2",
+        "1.4.0",
+        "1.4.1",
+        "1.4.2",
+        "1.4.3",
+        "1.4.4",
+        "1.4.5",
+        "1.5.0",
+    ]
+}
+
+PROTOTYPE_VERSIONS = {
+    ALLOWED: [
+        "1.7.1.0",
+        "1.7.2.0",
+    ]
+}
+
+UNDERSCORE_VERSIONS = {
+    ALLOWED: [
+        "1.1.4",
+        "1.1.5",
+        "1.1.7",
+        "1.2.0",
+        "1.2.1",
+        "1.2.2",
+        "1.2.3",
+        "1.2.4",
+        "1.3.0",
+        "1.3.1",
+        "1.3.2",
+        "1.3.3",
+        "1.4.0",
+        "1.4.1",
+        "1.4.2",
+        "1.4.3",
+        "1.4.4",
+        "1.5.0",
+        "1.5.1",
+        "1.5.2",
+        "1.6.0",
+        "1.7.0",
+        "1.8.0",
+        "1.8.1",
+        "1.8.2",
+        "1.8.3",
+    ]
 }
 
 
-# AngularJS (Google)
-get_pattern("angularjs",
-            "https://ajax.googleapis.com/ajax/libs/angularjs/%s/angular.js",
-            ANGULARJS_VERSIONS)
-get_pattern("angularjs",
-            "https://ajax.googleapis.com/ajax/libs/angularjs/%s/angular.min.js",
-            ANGULARJS_VERSIONS)
+def process(url, rule, file):
+    dest_folder = os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                               sys.argv[1], rule)
+
+    if not os.path.exists(dest_folder):
+        os.makedirs(dest_folder)
+
+    destination = os.path.join(dest_folder, file)
+    if os.path.exists(destination):
+        return
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        with open(destination, "wb") as code:
+            code.write(response.content)
+            print "Downloaded: {}".format(url)
+    except requests.exceptions.HTTPError as e:
+        global download_errors
+        download_errors.append((response.url, response.status_code, response.reason))
 
 
-# Backbone
-get_pattern("backbone",
-            "https://raw.githubusercontent.com/jashkenas/backbone/%s/backbone.js",
-            BACKBONE_VERSIONS)
-get_pattern("backbone",
-            "https://raw.githubusercontent.com/jashkenas/backbone/%s/backbone-min.js",
-            BACKBONE_VERSIONS)
-
-# Backbone.localStorage versions
-get_pattern("backbone.localStorage",
-            "https://raw.githubusercontent.com/jeromegn/Backbone.localStorage/v%s/backbone.localStorage.js",
-            BACKBONE_LOCALSTORAGE_VERSIONS)
-get_pattern("backbone.localStorage",
-            "https://raw.githubusercontent.com/jeromegn/Backbone.localStorage/v%s/backbone.localStorage-min.js",
-            BACKBONE_LOCALSTORAGE_VERSIONS)
-
-# Dojo Toolkit
-get_pattern("dojo",
-            "http://download.dojotoolkit.org/release-%s/dojo.js",
-            DOJO_VERSIONS)
-get_pattern("dojo",
-            "http://download.dojotoolkit.org/release-%s/dojo.js.uncompressed.js",
-            DOJO_VERSIONS)
-
-# EXT.js
-get_pattern("ext-core",
-            "https://ajax.googleapis.com/ajax/libs/ext-core/%s/ext-core.js",
-            EXT_VERSIONS)
-get_pattern("ext-debug",
-            "https://ajax.googleapis.com/ajax/libs/ext-core/%s/ext-core-debug.js",
-            EXT_VERSIONS)
-
-# jQuery
-get_pattern("jquery",
-            "http://code.jquery.com/jquery-%s.js",
-            JQUERY_VERSIONS)
-get_pattern("jquery",
-            "http://code.jquery.com/jquery-%s.min.js",
-            JQUERY_VERSIONS)
-
-# jQueryUI
-get_pattern("jqueryui",
-            "https://ajax.googleapis.com/ajax/libs/jqueryui/%s/jquery-ui.min.js",
-            JQUERYUI_VERSIONS)
-get_pattern("jqueryui",
-            "https://ajax.googleapis.com/ajax/libs/jqueryui/%s/jquery-ui.js",
-            JQUERYUI_VERSIONS)
-
-# moment.js
-get_pattern("moment",
-            "https://raw.githubusercontent.com/moment/moment/%s/moment.js",
-            MOMENTJS_VERSIONS)
-get_pattern("moment",
-            "https://raw.githubusercontent.com/moment/moment/%s/min/moment.min.js",
-            MOMENTJS_VERSIONS)
+def get_pattern(prefix, url_pattern, library):
+    for rule, versions in library.iteritems():
+        for version in versions:
+            url = url_pattern % version
+            process(url, rule, "%s.%s.%s" % (prefix, version, url.split("/")[-1]))
 
 
-# MooTools
-get_pattern("mootools",
-            "https://ajax.googleapis.com/ajax/libs/mootools/%s/mootools-yui-compressed.js",
-            MOOTOOLS_VERSIONS)
-get_pattern("mootools",
-            "https://ajax.googleapis.com/ajax/libs/mootools/%s/mootools.js",
-            MOOTOOLS_VERSIONS)
+def get_patterns():
+    # AngularJS
+    get_pattern("angularjs",
+                "https://code.angularjs.org/%s/angular.js",
+                ANGULARJS_VERSIONS)
+    get_pattern("angularjs",
+                "https://code.angularjs.org/%s/angular.min.js",
+                ANGULARJS_VERSIONS)
 
-# Prototype.js
-get_pattern("prototype",
-            "https://ajax.googleapis.com/ajax/libs/prototype/%s/prototype.js",
-            PROTOTYPE_VERSIONS)
+    # Backbone
+    get_pattern("backbone",
+                "https://raw.githubusercontent.com/jashkenas/backbone/%s/backbone.js",
+                BACKBONE_VERSIONS)
+    get_pattern("backbone",
+                "https://raw.githubusercontent.com/jashkenas/backbone/%s/backbone-min.js",
+                BACKBONE_VERSIONS)
 
-# Scriptaculous
-get_pattern("scriptaculous",
-            "https://ajax.googleapis.com/ajax/libs/scriptaculous/%s/scriptaculous.js",
-            SCRIPTACULOUS_VERSIONS)
+    # Dojo Toolkit
+    get_pattern("dojo",
+                "https://download.dojotoolkit.org/release-%s/dojo.js",
+                DOJO_VERSIONS)
+    get_pattern("dojo",
+                "https://download.dojotoolkit.org/release-%s/dojo.js.uncompressed.js",
+                DOJO_VERSIONS)
 
-# SWFObject
-get_pattern("swfobject",
-            "https://ajax.googleapis.com/ajax/libs/swfobject/%s/swfobject.js",
-            SWFOBJECT_VERSIONS)
-get_pattern("swfobject",
-            "https://ajax.googleapis.com/ajax/libs/swfobject/%s/swfobject_src.js",
-            SWFOBJECT_VERSIONS)
+    # jQuery
+    get_pattern("jquery",
+                "https://code.jquery.com/jquery-%s.js",
+                JQUERY_VERSIONS)
+    get_pattern("jquery",
+                "https://code.jquery.com/jquery-%s.min.js",
+                JQUERY_VERSIONS)
 
-# Underscore
-get_pattern("underscore",
-            "https://raw.github.com/documentcloud/underscore/%s/underscore.js",
-            UNDERSCORE_VERSIONS)
-get_pattern("underscore",
-            "https://raw.github.com/documentcloud/underscore/%s/underscore-min.js",
-            UNDERSCORE_VERSIONS)
+    # jQueryUI
+    get_pattern("jqueryui",
+                "https://code.jquery.com/ui/%s/jquery-ui.min.js",
+                JQUERYUI_VERSIONS)
+    get_pattern("jqueryui",
+                "https://code.jquery.com/ui/%s/jquery-ui.js",
+                JQUERYUI_VERSIONS)
 
-# Old-style YUI loader libraries:
-get_pattern("yui",
-            "http://yui.yahooapis.com/%s/build/yuiloader/yuiloader-min.js",
-            YUI_VERSIONS)
-get_pattern("yui",
-            "http://yui.yahooapis.com/%s/build/yuiloader/yuiloader.js",
-            YUI_VERSIONS)
+    # moment.js
+    get_pattern("moment",
+                "https://raw.githubusercontent.com/moment/moment/%s/moment.js",
+                MOMENTJS_VERSIONS)
+    get_pattern("moment",
+                "https://raw.githubusercontent.com/moment/moment/%s/min/moment.min.js",
+                MOMENTJS_VERSIONS)
 
-# New-style YUI libraries
-get_pattern("yui",
-            "http://yui.yahooapis.com/%s/build/yui-base/yui-base-min.js",
-            YUI_NEW_VERSIONS)
-get_pattern("yui",
-            "http://yui.yahooapis.com/%s/build/yui-base/yui-base.js",
-            YUI_NEW_VERSIONS)
+    # MooTools
+    get_pattern("mootools",
+                "https://ajax.googleapis.com/ajax/libs/mootools/%s/mootools-yui-compressed.js",
+                MOOTOOLS_VERSIONS)
+    get_pattern("mootools",
+                "https://ajax.googleapis.com/ajax/libs/mootools/%s/mootools.js",
+                MOOTOOLS_VERSIONS)
+
+    # Prototype.js
+    get_pattern("prototype",
+                "https://ajax.googleapis.com/ajax/libs/prototype/%s/prototype.js",
+                PROTOTYPE_VERSIONS)
+
+    # Underscore
+    get_pattern("underscore",
+                "https://raw.github.com/documentcloud/underscore/%s/underscore.js",
+                UNDERSCORE_VERSIONS)
+    get_pattern("underscore",
+                "https://raw.github.com/documentcloud/underscore/%s/underscore-min.js",
+                UNDERSCORE_VERSIONS)
+
+    CRYPTO_FILES = {
+        ALLOWED: ["aes", "cipher-core", "core", "enc-base64", "enc-utf16", "evpkdf",
+                  "format-hex", "hmac", "lib-typedarrays", "md5", "mode-cfb",
+                  "mode-ctr-gladman", "mode-ctr", "mode-ecb", "mode-ofb",
+                  "pad-ansix923", "pad-iso10126", "pad-iso97971", "pad-nopadding",
+                  "pad-zeropadding", "pbkdf2", "rabbit-legacy", "rabbit", "rc4",
+                  "ripemd160", "sha1", "sha224", "sha256", "sha3", "sha384",
+                  "sha512", "tripledes", "x64-core"]
+    }
+
+    get_pattern(
+        "crypto_js", "https://crypto-js.googlecode.com/svn/tags/3.1.2/src/%s.js",
+        CRYPTO_FILES)
 
 
-CRYPTO_FILES = {
-    ALLOWED: ["aes", "cipher-core", "core", "enc-base64", "enc-utf16",
-                 "evpkdf", "hmac", "md5", "mode-cfb", "mode-ctr", "mode-ecb",
-                 "mode-ofb", "pad-ansix923", "pad-iso10126", "pad-iso97971",
-                 "pad-nopadding", "pad-zeropadding", "pbkdf2", "rabbit", "rc4",
-                 "sha1", "sha224", "sha256", "sha384", "sha512", "tripledes",
-                 "x64-core"]
-}
-
-get_pattern(
-    "crypto_js", "http://crypto-js.googlecode.com/svn/tags/3.0.2/src/%s.js",
-    CRYPTO_FILES)
+print "Downloading third-party library files..."
+get_patterns()
+if download_errors:
+    for url, code, reason in download_errors:
+        print "Failed: {} is '{}' ({}).".format(url, reason, code)
+    print "Some files failed to download, please check the output above...Exiting."
+    sys.exit(1)
+print "Downloading third-party library files complete."
